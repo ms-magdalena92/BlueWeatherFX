0eb12650087ba922513435f1415e20ea6da3d838
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-05-30 16:38:10 +0200

    Render main view

--- src/main/java/blueWeather/App.java
+++ src/main/java/blueWeather/App.java
@@ -1,16 +1,28 @@
 package blueWeather;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(loadFXML("main"));
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
 
+    public static void main(String[] args) {
+        launch();
     }
 }
