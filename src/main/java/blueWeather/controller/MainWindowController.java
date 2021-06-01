fd593d62ee3c562687432c29472592bf65dca622
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-06-01 13:39:48 +0200

    Fetch current weather's conditons and initialize view

--- src/main/java/blueWeather/controller/MainWindowController.java
+++ src/main/java/blueWeather/controller/MainWindowController.java
@@ -1,13 +1,23 @@
 package blueWeather.controller;
 
+import blueWeather.model.CurrentWeatherConditions;
+import blueWeather.service.WeatherForecastFetcher;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import net.aksingh.owmjapis.api.APIException;
 
-public class MainWindowController extends BaseController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController extends BaseController implements Initializable {
 
     private static final String MAIN_VIEW_FILE_NAME = "main.fxml";
+
+    private static final String DEFAULT_CITY = "Warsaw";
 
     @FXML
     private AnchorPane currentLocationWeather;
@@ -39,7 +49,38 @@ public class MainWindowController extends BaseController {
     @FXML
     private AnchorPane targetLocationWeather;
 
+    CurrentWeatherConditions currentWeather;
+
+    private final WeatherForecastFetcher weatherForecastFetcher;
+
     public MainWindowController() {
         super(MAIN_VIEW_FILE_NAME);
+        weatherForecastFetcher = new WeatherForecastFetcher(DEFAULT_CITY);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            currentWeather = weatherForecastFetcher.fetchCurrentWeatherForecast();
+            if (currentWeather != null) {
+                setUpCurrentWeatherView();
+            } else {
+                //show error: data cannot be fully downloaded
+            }
+        } catch (APIException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUpCurrentWeatherView() {
+        currentLocation.setText(currentWeather.getCityName());
+        weatherIcon.setImage(new Image(currentWeather.getIconUrl()));
+        weatherIcon.setFitHeight(60);
+        weatherIcon.setFitWidth(60);
+        currentTemperature.setText(currentWeather.getTemperature());
+        weatherDescription.setText(currentWeather.getDescription());
+        humidity.setText(currentWeather.getHumidity());
+        pressure.setText(currentWeather.getPressure());
+        wind.setText(currentWeather.getWindSpeed());
     }
 }
