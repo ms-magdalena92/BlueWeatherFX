a37fe573afa010fa3c798ebde74b25f35ee43f54
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-06-05 13:52:25 +0200

    Create daily weathers model

---
+++ src/main/java/blueWeather/model/DailyWeatherConditions.java
@@ -0,0 +1,25 @@
+package blueWeather.model;
+
+import net.aksingh.owmjapis.model.param.WeatherData;
+
+public class DailyWeatherConditions extends WeatherBase {
+
+    private double dayTemperature;
+
+    private double nightTemperature;
+
+    public DailyWeatherConditions(WeatherData dayWeather, double dayTemperature, double nightTemperature) {
+        super(
+                dayWeather.getDateTime(),
+                dayWeather.getMainData().getPressure(),
+                dayWeather.getWeatherList().get(0).getMainInfo(),
+                dayWeather.getWeatherList().get(0).getIconLink()
+        );
+        this.dayTemperature = dayTemperature;
+        this.nightTemperature = nightTemperature;
+    }
+
+    public String getTemperature() {
+        return (int) dayTemperature + "\u00b0C" + "/" + (int) nightTemperature + "\u00b0C";
+    }
+}
