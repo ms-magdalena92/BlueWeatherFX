bfdbc1873647ba0f98b1616c9784538eee5b05d2
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-06-07 16:39:18 +0200

    Refactor the code by assigning each split pane's part a separate controller

---
+++ src/main/java/blueWeather/controller/CurrentLocationWeatherController.java
@@ -0,0 +1,36 @@
+package blueWeather.controller;
+
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CurrentLocationWeatherController extends WeatherBaseController {
+
+    public CurrentLocationWeatherController() {
+        super();
+        getCurrentLocation();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setUpWeatherViews();
+    }
+
+    @Override
+    protected void getLocation() {
+        getCurrentLocation();
+        clearAllViews();
+        setUpWeatherViews();
+    }
+
+    private void getCurrentLocation() {
+        try {
+            this.location = locationHandler.getCurrentLocationByIp();
+        } catch (IOException | InterruptedException e) {
+            clearAllViews();
+            generalError.setText("Sorry, your location could not be determined.");
+        }
+    }
+}
---
+++ src/main/java/blueWeather/controller/WeatherBaseController.java
@@ -0,0 +1,172 @@
+package blueWeather.controller;
+
+import blueWeather.model.CurrentWeatherConditions;
+import blueWeather.model.DailyWeatherConditions;
+import blueWeather.model.Location;
+import blueWeather.model.WeatherForecast;
+import blueWeather.service.LocationHandler;
+import blueWeather.service.WeatherForecastFetcher;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import net.aksingh.owmjapis.api.APIException;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WeatherBaseController implements Initializable {
+
+    protected Location location;
+
+    private final WeatherForecastFetcher weatherForecastFetcher;
+
+    protected final LocationHandler locationHandler;
+
+    private WeatherForecast weatherForecast;
+
+    @FXML
+    private Label currentLocation;
+
+    @FXML
+    private ImageView weatherIcon;
+
+    @FXML
+    private Label currentTemperature;
+
+    @FXML
+    private Label weatherDescription;
+
+    @FXML
+    private Label humidity;
+
+    @FXML
+    private Label pressure;
+
+    @FXML
+    private Label wind;
+
+    @FXML
+    private Label date;
+
+    @FXML
+    private HBox extendedForecast;
+
+    @FXML
+    private Label extendedForecastError;
+
+    @FXML
+    protected Label generalError;
+
+    @FXML
+    private Label currentWeatherError;
+
+    public WeatherBaseController() {
+        weatherForecastFetcher = new WeatherForecastFetcher();
+        locationHandler = new LocationHandler();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML
+    protected void getLocation() {
+    }
+
+    protected void clearAllViews() {
+        ObservableList<Node> panesChildren = FXCollections.concat(
+                currentLocation.getParent().getChildrenUnmodifiable(),
+                extendedForecast.getParent().getChildrenUnmodifiable()
+        );
+        setChildrenVisibility(panesChildren, false);
+        generalError.setText("");
+        currentWeatherError.setText("");
+        extendedForecastError.setText("");
+    }
+
+    protected void setUpWeatherViews() {
+        if (location != null) {
+            try {
+                weatherForecast = weatherForecastFetcher.fetchWeatherForecast(location.getCity());
+                showCurrentWeather();
+                showExtendedForecast();
+            } catch (APIException e) {
+                clearAllViews();
+
+                if (e.getCode() == 404 || e.getCode() == 400) {
+                    generalError.setText("City not found.");
+                } else {
+                    generalError.setText("Sorry, something went wrong.");
+                }
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void showExtendedForecast() {
+        if (!weatherForecast.getDailyWeatherConditions().isEmpty()) {
+            setUpExtendedForecastView();
+        } else {
+            extendedForecast.setVisible(false);
+            extendedForecastError.setText("Sorry, data could not be fully downloaded.");
+        }
+    }
+
+    private void showCurrentWeather() {
+        if (weatherForecast.getCurrentWeatherConditions() != null) {
+            setUpCurrentWeatherView();
+        } else {
+            setChildrenVisibility(currentLocation.getParent().getChildrenUnmodifiable(), false);
+            currentWeatherError.setText("Sorry, data could not be fully downloaded.");
+        }
+    }
+
+    private void setUpCurrentWeatherView() {
+        CurrentWeatherConditions currentWeather = weatherForecast.getCurrentWeatherConditions();
+
+        setChildrenVisibility(currentLocation.getParent().getChildrenUnmodifiable(), true);
+
+        currentLocation.setText(currentWeather.getCityName() + ", " + location.getCountry());
+        weatherIcon.setImage(new Image(currentWeather.getIconUrl()));
+        weatherIcon.setFitHeight(60);
+        weatherIcon.setFitWidth(60);
+        currentTemperature.setText(currentWeather.getTemperature());
+        weatherDescription.setText(currentWeather.getDescription());
+        humidity.setText(currentWeather.getHumidity());
+        pressure.setText(currentWeather.getPressure());
+        wind.setText(currentWeather.getWindSpeed());
+        date.setText(currentWeather.getDate());
+    }
+
+    private void setUpExtendedForecastView() {
+        extendedForecast.getChildren().clear();
+        extendedForecast.setVisible(true);
+
+        for (DailyWeatherConditions dailyWeatherConditions : weatherForecast.getDailyWeatherConditions()) {
+            VBox dayVBox = new VBox();
+            dayVBox.getChildren().addAll(
+                    new Label(dailyWeatherConditions.getDate()),
+                    new ImageView(new Image(dailyWeatherConditions.getIconUrl())),
+                    new Label(dailyWeatherConditions.getDescription()),
+                    new Label(dailyWeatherConditions.getTemperature()),
+                    new Label(dailyWeatherConditions.getPressure())
+            );
+
+            extendedForecast.getChildren().add(dayVBox);
+        }
+    }
+
+    private void setChildrenVisibility(ObservableList<Node> children, boolean visible) {
+        for (Node child : children) {
+            child.setVisible(visible);
+        }
+    }
+}
