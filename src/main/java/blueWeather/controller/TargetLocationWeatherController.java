a7e580488a730225eb8bfb3f8e02368a5b52c288
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-06-07 20:53:15 +0200

    Add autocompletion to target city input

--- src/main/java/blueWeather/controller/TargetLocationWeatherController.java
+++ src/main/java/blueWeather/controller/TargetLocationWeatherController.java
@@ -1,15 +1,32 @@
 package blueWeather.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import org.controlsfx.control.textfield.TextFields;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TargetLocationWeatherController extends WeatherBaseController implements Initializable {
 
+    @FXML
+    private TextField locationInput;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         clearAllViews();
         generalError.setText("Choose city");
+        setUpLocationPicker();
+    }
+
+    private void setUpLocationPicker() {
+        try {
+            TextFields.bindAutoCompletion(locationInput, locationHandler.getCityList().values());
+        } catch (IOException e) {
+            generalError.setText("Sorry, list of available city could not be initialized.");
+            e.printStackTrace();
+        }
     }
 }
