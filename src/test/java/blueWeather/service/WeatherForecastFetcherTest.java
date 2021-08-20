d7bd96ceff552fe6d88b1e0200265e21c1848f7a
Magdalena Slomiany <magdalena.slomiany.programista@gmail.com>
2021-08-20 17:25:01 +0200

    Make static variable public to make it usable in tests

--- src/test/java/blueWeather/service/WeatherForecastFetcherTest.java
+++ src/test/java/blueWeather/service/WeatherForecastFetcherTest.java
@@ -41,7 +41,7 @@ public class WeatherForecastFetcherTest {
         List<DailyWeatherConditions> dailyWeatherConditions = weatherForecast.getDailyWeatherConditions();
 
         //then
-        assertThat(dailyWeatherConditions, hasSize(4));
+        assertThat(dailyWeatherConditions, hasSize(WeatherForecastFetcher.WEATHER_FORECAST_DAYS_COUNT));
         assertThat(currentWeatherConditions.getCityName(), equalTo(cityName));
         assertThat(currentWeatherConditions.getHumidity(), equalTo((int) Math.round(OwmApiStub.HUMIDITY) + "%"));
         assertThat(dailyWeatherConditions.get(0).getPressure(), equalTo(OwmApiStub.PRESSURE + "hPa"));
